3a8fb374a31a6cc1f2746fd3d94a944faeefc381
Jacob Dye <jacobdye@pop-os.localdomain>
2022-02-01 16:05:43 -0500

    added models

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const commentSchema = mongoose.Schema(
+    {
+        comment: { type: String, required: true, trim: true},
+    },
+    { timestamps: true }
+);
+
+// const favoriteSchema = mongoose.Schema(
+//     {
+//         favorite:
+//     }
+// )
+
+
+const userSchema = mongoose.Schema(
+    {
+        userName: { type: String, required: true, trim: true},
+        firstName: { type: String, required: true, trim: true},
+        lastName: { type: String, required: true, trim: true},
+        email: { type: String, required: true, trim: true},
+        password: { type: String, required: true, trim: true},
+        confirmPassword: { type: String, required: true, trim: true},
+        avatar: { type: String, required: true, trim: true},
+        comments: [commentSchema],
+        // favorites: [favoriteSchema],
+        isAdmin: false,
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema, "users")
+export const Comment = mongoose.model("Comment", commentSchema, "comments")
