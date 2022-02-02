e66ff7655e0638da2e16048cf5cdb66e44c1e611
Jacob Dye <jacobdye@pop-os.localdomain>
2022-02-01 20:59:10 -0500

    Fixed find user

--- src/utilities/securePassword.js
+++ src/utilities/securePassword.js
@@ -3,6 +3,6 @@ import bcrypt from 'bcryptjs'
 export const securePassword = async password => {
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-  console.log(hashedPassword)
+  // console.log(hashedPassword)
   return hashedPassword;
 };
