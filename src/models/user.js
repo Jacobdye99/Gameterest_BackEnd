b03337207bd26eb42500ebd056198c07c42d1de0
Jacob Dye <jacobdye@pop-os.localdomain>
2022-02-01 20:51:06 -0500

    genSaltSync

--- src/models/user.js
+++ src/models/user.js
@@ -20,7 +20,7 @@ const userSchema = mongoose.Schema(
         firstName: { type: String, required: true, trim: true},
         lastName: { type: String, required: true, trim: true},
         email: { type: String, required: true, trim: true},
-        password: { type: String, required: true, trim: true},
+        password: {type: String, required: true, trim: true},
         confirmPassword: { type: String, required: true, trim: true},
         avatar: { type: String, required: true, trim: true},
         comments: [commentSchema],
